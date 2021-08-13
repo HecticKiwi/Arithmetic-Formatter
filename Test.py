21b0497366590464bf74d1a65500454d85e3d6fd
HecticKiwi <88013638+HecticKiwi@users.noreply.github.com>
2021-08-12 20:22:59 -0600

    Update

--- Test.py
+++ Test.py
@@ -1,14 +1,27 @@
 import re
 
-def arithmetic_arranger(problems):
-    row1, row2 = '', ''
+def arithmetic_arranger(problems, show_answer=False):
+    row1, row2, row3, row4= '', '', '', ''
 
     for problem in problems:
         num1, num2 = re.findall('\d+', problem)
-        row1 += (f'{num1:>5}')
         operator = re.findall('[+-]', problem)[0]
-        row2 += (f'{operator} + " " + {num2:4}')
 
-        return row1, row2
+        width = max(len(num1), len(num2)) + 2
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+        row1 += (num1.rjust(width))
+        row2 += (operator + num2.rjust(width - 1))
+        row3 += "-" * width
+        row4 += str(eval(problem)).rjust(width)
+
+        row1 += '    '
+        row2 += '    '
+        row3 += '    '
+        row4 += '    '
+
+    if show_answer:
+        return '\n'.join([row1, row2, row3, row4])
+    else:
+        return '\n'.join([row1, row2, row3])
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
