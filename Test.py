b112b496a564b32082c5f0da9d9d461315bd499b
HecticKiwi <88013638+HecticKiwi@users.noreply.github.com>
2021-08-12 20:54:17 -0600

    Update

--- Test.py
+++ Test.py
@@ -5,7 +5,17 @@ def arithmetic_arranger(problems, show_answer=False):
 
     for problem in problems:
         num1, num2 = re.findall('\d+', problem)
-        operator = re.findall('[+-]', problem)[0]
+        try:   
+            operator = re.findall('[+-]', problem)[0]
+        except:
+            return "Error: Operator must be '+' or '-'."
+
+        if len(problems) > 5:
+            return "Error: Too many problems."
+        elif not row1.isnumeric() and row2.isnumeric():
+            return "Error: Numbers must only contain digits."
+        elif len(num1) > 4 or len(num2) > 4:
+            return "Error: Numbers cannot be more than four digits."
 
         width = max(len(num1), len(num2)) + 2
 
@@ -20,8 +30,10 @@ def arithmetic_arranger(problems, show_answer=False):
         row4 += '    '
 
     if show_answer:
-        return '\n'.join([row1, row2, row3, row4])
+        arranged_problems = '\n'.join([row1, row2, row3, row4])
     else:
-        return '\n'.join([row1, row2, row3])
+        arranged_problems = '\n'.join([row1, row2, row3])
+    
+    return arranged_problems
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
